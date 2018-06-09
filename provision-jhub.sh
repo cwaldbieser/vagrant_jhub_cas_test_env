9e4af366df5bf75d29ca5f18e09a7dca08a8a99b
Carl Waldbieser <cwaldbieser@gmail.com>
2018-06-09 16:43:36 -0400

    Upgrade pip to latest version.

--- provision-jhub.sh
+++ provision-jhub.sh
@@ -6,6 +6,7 @@
 /usr/bin/apt-get -y install python3-pip
 /usr/bin/apt-get -y install -y npm nodejs-legacy
 /usr/bin/npm install -g configurable-http-proxy
+/usr/bin/python3 -m pip install -U pip
 /usr/bin/python3 -m pip install jupyterhub
 # Install the python3 Jupyter notebook.
 /usr/bin/python3 -m pip install notebook
